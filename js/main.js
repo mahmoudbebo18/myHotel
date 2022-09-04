ffe2c8bb23330c7b6db40749b4eef8da98e48c23
mahmoudElkhateb18 <m.elkhateb@exp-sa.com>
2022-09-04 02:04:35 +0200

    upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const city = document.getElementById("myCity")
+city.addEventListener('change', function handleChange(event) {
+    const cityVal = event.target.value;
+    console.log(cityVal);
+})
+$(document).ready(function () {
+    $('.submit button').click(function (e) {
+        e.preventDefault();
+        const countryCode = $('#basic-addon3').text();
+        const phone = $('#basic-url').val();
+        const number = countryCode + phone
+        const gender = $("input[type='radio'][name='gender']:checked").val();
+        const api = 'https://jsonplaceholder.typicode.com/users';
+        $.post(api, {
+
+        })
+    })
+
+    // fetch(api)
+    //     .then(response => response.json())
+    //     .then(json => console.log(json))
+
+})
+
